99d917979e12a36188ff3112941b5a15ee18a788
MarKobe1990 <masiyuan268@outlook.com>
2022-10-19 13:42:55 +0800

    rebuild the dataset

---
+++ SODSemanticTools/RebuildDataset_imgnet21k.py
@@ -0,0 +1,246 @@
+import datetime
+
+import cv2
+import numpy as np
+from tensorboardX import SummaryWriter
+from treelib import Tree
+import urllib
+from argparse import Namespace
+import torch
+from timm.data import resolve_data_config
+from timm.data.transforms_factory import create_transform
+from src_files.semantic.semantics import ImageNet21kSemanticSoftmax
+import timm
+from myTools.data import GetDataset
+from tqdm import tqdm
+from matplotlib import pyplot as plt
+from torchvision import transforms
+from PIL import Image
+from SODSemanticTools.WordNetTool import build_tree, get_object_hypernym_paths
+import myTools.imageUtil as utils
+import os.path as osp
+import os
+
+from src_files.semantic.semantics import ImageNet21kSemanticSoftmax
+
+
+def init_my_model(metadata_file, checkpoint_file):
+    # 配置文件
+    args = Namespace()
+    args.tree_path = metadata_file
+    semantic_softmax_processor = ImageNet21kSemanticSoftmax(args)
+    model = timm.create_model('vit_base_patch16_224_miil_in21k', pretrained=False, checkpoint_path=checkpoint_file)
+    model.eval()
+    return model, semantic_softmax_processor
+
+
+def draw_max_rec_of_mask(gt_path, image_path):
+    gt_mask = cv2.imread(gt_path)
+    thresh = cv2.Canny(gt_mask, 128, 256)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    img_origin = cv2.imread(image_path)
+    img_copy = np.copy(img_origin)
+    x1 = []
+    y1 = []
+    x2 = []
+    y2 = []
+    for contour in contours:
+        # 找到边界坐标
+        x, y, w, h = cv2.boundingRect(contour)  # 计算点集最外面的矩形边界
+        # 因为这里面包含了，图像本身那个最大的框，所以用了if，来剔除那个图像本身的值。
+        # if x != 0 and y != 0 and w != gt_mask.shape[1] and h != gt_mask.shape[0]:
+        # if w != gt_mask.shape[1] and h != gt_mask.shape[0]:
+        # 左上角坐标和右下角坐标
+        # 如果执行里面的这个画框，就是分别来画的，
+        # cv2.rectangle(origin_gt, (x, y), (x + w, y + h), (0, 255, 0), 1)
+        x1.append(x)
+        y1.append(y)
+        x2.append(x + w)
+        y2.append(y + h)
+    x11 = min(x1)
+    y11 = min(y1)
+    x22 = max(x2)
+    y22 = max(y2)
+    white = [255, 255, 255]
+    for col in range(x11, x22):
+        for row in range(y11, y22):
+            gt_mask[row, col] = white
+    rectangle_mask_gray = cv2.cvtColor(gt_mask, cv2.COLOR_RGB2GRAY)
+    out_origin_size = cv2.bitwise_and(img_copy, img_copy, mask=rectangle_mask_gray)
+    return gt_mask, out_origin_size, img_copy[y11:y22, x11:x22]
+
+
+def mask_object_count_and_ratio(gt_path):
+    gt_mask = cv2.imread(gt_path, 0)
+    retval, labels, stats, centroids = cv2.connectedComponentsWithStats(gt_mask, connectivity=8)
+    ratio_object = (1.0 - float(stats[0][4]) / float(gt_mask.size))
+    return retval, labels, stats, centroids, ratio_object
+
+
+def input_tensor_to_labels(tensor, model, semantic_softmax_processor):
+    labels = []
+    labels_top1_prob = []
+    labels_and_prob = {}
+    with torch.no_grad():
+        logits = model(tensor)
+        semantic_logit_list = semantic_softmax_processor.split_logits_to_semantic_logits(logits)
+
+        # scanning hirarchy_level_list
+        for i in range(len(semantic_logit_list)):
+            logits_i = semantic_logit_list[i]
+
+            # generate probs
+            probabilities = torch.nn.functional.softmax(logits_i[0], dim=0)
+            top1_prob, top1_id = torch.topk(probabilities, 1)
+
+            if top1_prob > 0.1:
+                top_class_number = semantic_softmax_processor.hierarchy_indices_list[i][top1_id[0]]
+                top_class_name = semantic_softmax_processor.tree['class_list'][top_class_number]
+                top_class_description = semantic_softmax_processor.tree['class_description'][top_class_name]
+                labels.append(top_class_description)
+                top1_prob_float = float(top1_prob).__round__(5)
+                labels_and_prob[top_class_description] = top1_prob_float
+                labels_top1_prob.append(top1_prob_float)
+    labels_top1_prob_sorted_list = sorted(labels_and_prob.items(), key=lambda x: x[1])
+    return labels, labels_top1_prob, dict(labels_top1_prob_sorted_list)
+
+
+def rebuild_masked_dataset(dataset_dic, model, semantic_softmax_processor):
+    data_set = GetDataset(dataset_dic.get("image_root"), dataset_dic.get("depth_root"),
+                          dataset_dic.get("gt_root"))
+    pbar = tqdm(range(len(data_set)), desc="rebuild_dataset:", unit='img')
+    date_str = datetime.datetime.now().strftime('%Y-%m-%d-%H:%M')
+    save_dir = "../testResults/" + date_str
+    dataset_writer = SummaryWriter(save_dir + "/log", comment="log")
+    gt_dir = osp.join(save_dir, 'data', 'gt')
+    mask_origin_dir = osp.join(save_dir, 'data', 'mask_origin')
+    mask_resize_dir = osp.join(save_dir, 'data', 'mask_resize')
+    merge_img_dir = osp.join(save_dir, 'data', 'merge_img')
+    if not osp.isdir(gt_dir):
+        os.makedirs(gt_dir)
+    if not osp.isdir(mask_origin_dir):
+        os.makedirs(mask_origin_dir)
+    if not osp.isdir(mask_resize_dir):
+        os.makedirs(mask_resize_dir)
+    if not osp.isdir(merge_img_dir):
+        os.makedirs(merge_img_dir)
+    log_file_loc = save_dir + '/0_output_log.txt'
+    config = resolve_data_config({}, model=model)
+    transform = create_transform(**config)
+    word_tree = Tree()
+    error_list = []
+    for idx in pbar:
+        name, image_path, depth_path, gt_path, img_array = data_set.load_data(idx)
+        # 根据mask切割
+        gt_mask, out_origin_size, img_copy_resize = draw_max_rec_of_mask(gt_path, image_path)
+        # 统计联通区域和显著性物体占比
+        retval, labels, stats, centroids, ratio_object = mask_object_count_and_ratio(gt_path)
+        img_resize_salient_object_path = osp.join(mask_resize_dir, name)
+        img_mask_origin_path = osp.join(mask_origin_dir, name)
+        cv2.imwrite(osp.join(gt_dir, name[:-4] + '.png'), gt_mask)
+        cv2.imwrite(img_mask_origin_path, out_origin_size)
+        cv2.imwrite(img_resize_salient_object_path, img_copy_resize)
+        # 原图像的结果
+        origin_img = Image.open(image_path).convert('RGB')
+        origin_tensor = transform(origin_img).unsqueeze(0)
+        # 切割后的
+        masked_img = Image.open(img_resize_salient_object_path).convert('RGB')
+        masked_tensor = transform(masked_img).unsqueeze(0)
+        #处理原图像
+        origin_labels, origin_labels_top1_prob, origin_labels_and_prob = input_tensor_to_labels(origin_tensor, model, semantic_softmax_processor)
+        masked_labels, masked_labels_top1_prob, masked_labels_and_prob = input_tensor_to_labels(masked_tensor, model, semantic_softmax_processor)
+        top_class_result_origin = origin_labels_and_prob.popitem()
+        top_class_result_masked = masked_labels_and_prob.popitem()
+        if top_class_result_origin[0] == top_class_result_masked[0]:
+            same_flag = 'T'
+        else:
+            same_flag = 'F'
+        hparam_dic = {
+            "file_pth": image_path,
+            "pred_class": origin_labels.__str__(),
+            "pred_score": origin_labels_top1_prob.__str__(),
+            "pred_class_salient_object": masked_labels.__str__(),
+            "pred_score_salient_object": masked_labels_top1_prob.__str__(),
+            "same_flag": same_flag
+        }
+        metric_dic = {
+            "object_count": retval - 1,
+            "object_ratio": ratio_object
+        }
+
+        build_tree(top_class_result_masked[0], word_tree, image_path, error_list)
+
+        dataset_writer.add_hparams(hparam_dic, metric_dic, name='log/' + image_path)
+        merge_img_list = [image_path,
+                          depth_path,
+                          gt_path,
+                          img_mask_origin_path]
+        tag_dic = {
+            'class_name': top_class_result_origin[0],
+            'pred_socre': top_class_result_origin[1],
+            'ob_class_name': top_class_result_masked[0],
+            'ob_pred_socre': top_class_result_masked[1],
+            'object_ratio': ratio_object,
+            'object_count': retval - 1,
+            'same_flag': same_flag,
+            'name': name[:-4]
+        }
+        utils.merge_image_file_name_return_tensor(merge_img_list, name[:-4] + '_merge.png', merge_img_dir,
+                                                               len(merge_img_list), tag_dic)
+        # dataset_writer.add_image(dataset_dic["dataset_name"], image_grid, global_step=idx)
+
+    if os.path.isfile(log_file_loc):
+        logger = open(log_file_loc, 'a')
+    else:
+        logger = open(log_file_loc, 'w')
+        # 写入测试
+        logger.write(error_list.__str__() + "\n")
+    logger.flush()
+    word_tree.save2file(save_dir + '/tree_note_count.txt', data_property='count')
+    word_tree.save2file(save_dir + '/tree_note.txt')
+    word_tree.to_graphviz(filename=save_dir + '/tree_graphviz')
+
+
+
+if __name__ == '__main__':
+    # conformer
+    metadata_file = '../myDocument/imagenet21k_miil_tree.pth'
+    # 权重文件参数路径
+    checkpoint_file = '../checkpoints/vit_base_patch16_224_miil_21k.pth'
+
+    dataset_dic_train = {
+        'image_root': '../data/COME15K/train/imgs_right/',
+        'gt_root': '../data/COME15K/train/gt_right/',
+        'depth_root': '../data/COME15K/train/depths/',
+        'dataset_name': 'come15k_train'
+    }
+
+    dataset_dic_val_e = {
+        'image_root': '../data/COME15K/val/' + 'COME-E' + '/RGB/',
+        'gt_root': '../data/COME15K/val/' + 'COME-E' + '/GT/',
+        'depth_root': '../data/COME15K/val/' + 'COME-E' + '/depths/',
+        'dataset_name': 'come15k_val_easy'
+    }
+    dataset_dic_val_h = {
+        'image_root': '../data/COME15K/val/' + 'COME-H' + '/RGB/',
+        'gt_root': '../data/COME15K/val/' + 'COME-H' + '/GT/',
+        'depth_root': '../data/COME15K/val/' + 'COME-H' + '/depths/',
+        'dataset_name': 'come15k_val_hard'
+    }
+    dataset_dic_test_e = {
+        'image_root': '../data/COME15K/test/' + 'COME-E' + '/RGB/',
+        'gt_root': '../data/COME15K/test/' + 'COME-E' + '/GT/',
+        'depth_root': '../data/COME15K/test/' + 'COME-E' + '/depths/',
+        'dataset_name': 'come15k_test_easy'
+    }
+    dataset_dic_test_h = {
+        'image_root': '../data/COME15K/test/' + 'COME-H' + '/RGB/',
+        'gt_root': '../data/COME15K/test/' + 'COME-H' + '/GT/',
+        'depth_root': '../data/COME15K/test/' + 'COME-H' + '/depths/',
+        'dataset_name': 'come15k_test_hard'
+    }
+    model, semantic_softmax_processor = init_my_model(metadata_file, checkpoint_file)
+    # 标注图像类型
+    # rebuild_dataset(dataset_dic_train, model)
+    # 切割图像类型并标注类型和显著图像占比
+    rebuild_masked_dataset(dataset_dic_train, model, semantic_softmax_processor)
---
+++ myTools/imageUtil.py
@@ -0,0 +1,169 @@
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import torch
+import torchvision.utils as utils
+import torchvision.transforms as transforms
+from PIL import ImageOps
+import os.path as osp
+import time
+from math import *
+
+
+# tensor chanel BGR
+def merge_image_tensor_BGR(img_np_list, file_name, save_dir, n_row, tag=None):
+    grid = utils.make_grid(torch.cat([img_RGB, img_RGB, img_RGB, img_RGB], dim=0))
+    print(grid.size())
+    input_tensor = grid.clone().detach().to(torch.device('cpu'))  # 到cpu
+    utils.save_image(input_tensor, file_name)
+
+
+def merge_image_np(img_np_list, file_name, save_dir, n_row, tag=None):
+    img_tensor_list = []
+    for img_np in img_np_list:
+        img_tensor = transforms.ToTensor()(img_np).unsqueeze(dim=0)
+        img_tensor_list.append(img_tensor)
+    image_grid = utils.make_grid(torch.cat(img_tensor_list, dim=0), n_row).clone().detach().to(torch.device('cpu'))
+    utils.save_image(image_grid, save_dir + file_name)
+
+
+def merge_image_file_name(file_name_list, merge_img_name, save_dir, n_row, tag_dic=None):
+    img_tensor_list = []
+    shape = cv2.imread(file_name_list[0]).shape
+    for file_name in file_name_list:
+        img = cv2.imread(file_name)
+        if img.shape != shape:
+            img = cv2.resize(img, (shape[1], shape[0]))
+        img_np = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        if file_name == file_name_list[-1]:
+            if tag_dic is not None:
+                i = 0
+                for key, value in tag_dic.items():
+                    try:
+                        text_img = '{0:<8} : {1:>6.4f}'.format(key, value)
+                    except:
+                        text_img = '{0:<8} : {1:>6}'.format(key, value)
+                    # !图片，添加的文字，左上角坐标，字体，字体大小，颜色，字体粗细
+                    cv2.putText(img_np, text_img, (10, 25 + 20 * i), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
+                    i = i + 1
+        img_tensor = transforms.ToTensor()(img_np).unsqueeze(dim=0)
+        img_tensor_list.append(img_tensor)
+    image_grid = utils.make_grid(torch.cat(img_tensor_list, dim=0), n_row, pad_value=255).clone().detach().to(
+    torch.device('cpu'))
+    utils.save_image(image_grid, save_dir + merge_img_name)
+
+def merge_image_file_name_return_tensor(file_name_list, merge_img_name, save_dir, n_row, tag_dic=None):
+    img_tensor_list = []
+    shape = cv2.imread(file_name_list[0]).shape
+    for file_name in file_name_list:
+        img = cv2.imread(file_name)
+        if img.shape != shape:
+            img = cv2.resize(img, (shape[1], shape[0]))
+        img_np = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        if file_name == file_name_list[-1]:
+            if tag_dic is not None:
+                i = 0
+                for key, value in tag_dic.items():
+                    try:
+                        text_img = '{0:<8} : {1:>10.4f}'.format(key, value)
+                    except:
+                        text_img = '{0:<8} : {1:>10}'.format(key, value)
+                    # !图片，添加的文字，左上角坐标，字体，字体大小，颜色，字体粗细
+                    cv2.putText(img_np, text_img, (10, 25 + 20 * i), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
+                    i = i + 1
+        img_tensor = transforms.ToTensor()(img_np).unsqueeze(dim=0)
+        img_tensor_list.append(img_tensor)
+    image_grid = utils.make_grid(torch.cat(img_tensor_list, dim=0), n_row, pad_value=255).clone().detach().to(
+    torch.device('cpu'))
+    utils.save_image(image_grid, save_dir + '/' + merge_img_name)
+    return image_grid
+
+def look_img(img):
+    '''opencv读入图像格式为BGR，matplotlib可视化格式为RGB，因此需将BGR转RGB'''
+    img_RGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    plt.imshow(img_RGB)
+    plt.show()
+
+
+def inverse_color(image):
+    height, width = image.shape
+    img2 = image.copy()
+
+    for i in range(height):
+        for j in range(width):
+            img2[i, j] = (255 - image[i, j])
+    return img2
+
+
+if __name__ == "__main__":
+    # test = torch.rand((10, 1, 320, 320))
+    # test = test[:, 0, :, :].unsqueeze(dim=1)
+    # print(test.shape)
+    # image = cv2.imread("data/NJU2K/000001_left.jpg")
+    # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    # img_RGB = transforms.ToTensor()(image).unsqueeze(dim=0)
+    # print(img_RGB.size())
+    # grid = utils.make_grid(torch.cat([img_RGB, img_RGB, img_RGB, img_RGB], dim=0))
+    # print(grid.size())
+    # input_tensor = grid.clone().detach().to(torch.device('cpu'))  # 到cpu
+    # utils.save_image(input_tensor, "out_cv.jpg")
+    # cv2.imwrite("merge_image.jpg", image)
+
+    # look_img(image)
+    # image_list = [image, image, image, image]  # 这里存放你的图片帧列表
+    # big_image = merge_image(image_list, 2, 2)  # x_num为行数、y_num为列数，x与y的积为图像列表的长度
+    # cv2.imwrite("merge_image.jpg", big_image)
+    # src = './data/COME15K/test/COME-E/depths/COME_Easy_1007.png'
+    # depth = cv2.imread(src, 0)
+    # depth_rev = inverse_color(depth)
+    # eq_img = cv2.equalizeHist(depth)
+    # clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(16, 16))
+    # cl1 = clahe.apply(depth_rev)
+    # depth_rev_enhance = cv2.convertScaleAbs(depth_rev, 6050, 1)
+
+    # depth_resize=cv2.resize(depth, (360, 640))
+    # cv2.imshow('depth', depth)
+    # cv2.imshow('depth_resize', depth_resize)
+    # cv2.imshow('depth_rev', depth_rev)
+    # cv2.imshow('eq_img', eq_img)
+    # cv2.imshow('cl1', cl1)
+    # key = cv2.waitKey(0)
+
+
+    gt_mask = cv2.imread('../COME_Train_15.png', 0)
+    # thresh = cv2.Canny(gt_mask, 128, 256)
+    # cv2.imshow('thresh', thresh)
+    # key = cv2.waitKey(0)
+    retval, labels, stats, centroids = cv2.connectedComponentsWithStats(gt_mask, connectivity=8)
+    print(retval)
+    ratio_object = (1.0 - float(stats[0][4])/float(gt_mask.size))
+
+    # contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # x1 = []
+    # y1 = []
+    # x2 = []
+    # y2 = []
+    # for contour in contours:
+    #     # 找到边界坐标
+    #     x, y, w, h = cv2.boundingRect(contour)  # 计算点集最外面的矩形边界
+    #     print(x, y, w, h)
+    #     # 因为这里面包含了，图像本身那个最大的框，所以用了if，来剔除那个图像本身的值。
+    #     if x != 0 and y != 0 and w != gt_mask.shape[1] and h != gt_mask.shape[0]:
+    #         # 左上角坐标和右下角坐标
+    #         # 如果执行里面的这个画框，就是分别来画的，
+    #         # cv2.rectangle(origin_gt, (x, y), (x + w, y + h), (0, 255, 0), 1)
+    #         x1.append(x)
+    #         y1.append(y)
+    #         x2.append(x + w)
+    #         y2.append(y + h)
+    # x11 = min(x1)
+    # y11 = min(y1)
+    # x22 = max(x2)
+    # y22 = max(y2)
+    # white = [255, 255, 255]
+    # for col in range(x11, x22):
+    #     for row in range(y11, y22):
+    #         gt_mask[row, col] = white
+    # # rect = cv2.boundingRect(contours[1])
+    # cv2.imshow('gt_mask', gt_mask)
+    # key = cv2.waitKey(0)
